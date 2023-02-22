14690b136f0d34c991d65a7172c5cf2d95a8f840
Reynhart <906347@telkom.co.id>
2023-02-22 18:08:09 +0700

    update websocket handling

--- cmd/main.go
+++ cmd/main.go
@@ -2,33 +2,41 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
+	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"golang.org/x/net/websocket"
+)
+
+var (
+	upgrader = websocket.Upgrader{}
 )
 
 func hello(c echo.Context) error {
-	websocket.Handler(func(ws *websocket.Conn) {
-		defer ws.Close()
-		for {
-			// Write
-			err := websocket.Message.Send(ws, "Hello, Client!")
-			if err != nil {
-				c.Logger().Error(err)
-			}
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		return err
+	}
+	defer ws.Close()
 
-			// Read
-			msg := ""
-			err = websocket.Message.Receive(ws, &msg)
-			if err != nil {
-				c.Logger().Error(err)
-			}
-			fmt.Printf("%s\n", msg)
+	// Welcome message
+	err = ws.WriteMessage(websocket.TextMessage, []byte("Connection successful"))
+	if err != nil {
+		c.Logger().Error(err)
+	}
+
+	for {
+
+		// Read
+		_, msg, err := ws.ReadMessage()
+		if err != nil {
+			c.Logger().Error(err)
 		}
-	}).ServeHTTP(c.Response(), c.Request())
-	return nil
+		fmt.Printf("%s\n", msg)
+	}
 }
 
 func main() {
